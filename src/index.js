3bde98389e6afff0ebdfac40a0f6f32a234bd3fd
AlexandrKov <mulah1988@mail.ru>
2024-01-06 22:41:27 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const board = document.querySelector(".gameBoard");
-    const goblin = document.querySelector(".goblin");
-    let prevCell;
+import "./css/style.css";
 
-    for(let i = 0; i < 16; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        board.appendChild(cell)
-    }
-
-    const startPosition = () => {
-        const cells = document.querySelectorAll(".cell");
-        const randomCell = cells[Math.floor(Math.random() * cells.length)]
-        randomCell.appendChild(goblin)
-        prevCell = randomCell;
-    }
-    startPosition()
-
-    const game = () => {
-        const cells = document.querySelectorAll(".cell");
-        let a = Math.floor(Math.random() * cells.length);
-        const randomCell = () => {
-            return a = Math.floor(Math.random() * cells.length);
-        }
-        if(a !== prevCell) {
-            cells[a].appendChild(goblin);
-            prevCell = a;
-        } else {
-            randomCell()
-        }
-    }
-
-    setInterval(game, 2000)
-})
+import "./js/app";
